69dc9fa6f8575e92b266644e1876e15a4e6a3609
Gabriela Guedes <zooiv3rde@gmail.com>
2020-08-10 21:10:28 -0300

    lines

---
+++ src/lines/TesteDaFila.java
@@ -0,0 +1,17 @@
+package lines;
+
+public class TesteDaFila {
+	
+	public static void main(String[] args) {
+		Fila fila = new Fila();
+		
+		fila.adiciona("Gaby Guedes");
+		fila.adiciona("Paraíba");
+		
+		System.out.println(fila);
+		
+		//Queue<String> filaJavva = new LinkedList<String>();
+		
+	}
+
+}
